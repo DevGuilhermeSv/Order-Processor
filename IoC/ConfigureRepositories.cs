101fdb883a6b0846c7416d408f163673909921bd
Guilherme Rodrigues <development.guilherme@gmail.com>
2025-05-26 09:17:58 -0300

    Add IOrderService to IoC container and project reference for Application

--- IoC/ConfigureRepositories.cs
+++ IoC/ConfigureRepositories.cs
@@ -1,3 +1,5 @@
+using Application.Interfaces;
+using Application.Services;
 using Domain.Entities;
 using Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,6 +10,7 @@ public static class ConfigureRepositories
 {
     public static void Repositories(this IServiceCollection serviceCollection)
     {
+        serviceCollection.AddScoped<IOrderService, OrderService>();
         serviceCollection.AddSingleton<IOrderRepository, OrderRepository>();
         serviceCollection.AddSingleton<OrderContext<Order>>();
     }
